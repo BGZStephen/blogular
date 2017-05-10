ec9e807bf99e5cfbc53c01640256558031cd3100
Stephen Wright <sjw948@gmail.com>
2017-05-10 17:03:11 +0100

    added base routes and created base pages

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { router } from "./app.routes"
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BlogHomeComponent } from './pages/blog/blog-home.component';
+import { BlogIndividualComponent } from './pages/blog/blog-individual.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    BlogHomeComponent,
+    BlogIndividualComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    router
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-src/src/app/pages/blog/blog-individual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-individual',
+  templateUrl: './blog-individual.component.html',
+  styleUrls: ['./blog-individual.component.scss']
+})
+export class BlogIndividualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
