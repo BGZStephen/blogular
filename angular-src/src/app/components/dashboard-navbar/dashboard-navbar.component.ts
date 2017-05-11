51e5f85df7d9dbd7e0163116c98eac72c615eeca
Stephen Wright <sjw948@gmail.com>
2017-05-11 11:43:30 +0100

    built out responsive navbar component for dashboard view

--- angular-src/src/app/components/dashboard-navbar/dashboard-navbar.component.ts
+++ angular-src/src/app/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -12,4 +12,24 @@ export class DashboardNavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  activeSubMenu: number = -1;
+
+  toggleMenu() {
+    let navbar = document.getElementsByClassName("navbar-output-container")[0]
+    navbar.classList.toggle("navbar-visible");
+  }
+
+  toggleSubMenu(index) {
+    let topMenus = document.getElementsByClassName("navbar-top-level-item")
+    let topMenu = topMenus[index]
+    let subMenu = topMenu.getElementsByTagName("ul")[0]
+
+    if(index == this.activeSubMenu) {
+      subMenu.style.height = "0px";
+      this.activeSubMenu = -1
+    } else {
+      subMenu.style.height = 45 * (subMenu.children.length) + "px";
+      this.activeSubMenu = index
+    }
+  }
 }
